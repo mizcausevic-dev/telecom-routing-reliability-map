b6da746ceb68c4e58d3d495896be905abb58e514
fknmiz <150495873+fknmiz@users.noreply.github.com>
2026-06-08 16:33:41 -0400

    Enrich public evidence surface

--- scripts/prerender.ts
+++ scripts/prerender.ts
@@ -7,3 +7,5 @@ mkdirSync("site", { recursive: true });
 writeFileSync("site/index.html", renderPage(input));
 writeFileSync("site/robots.txt", "User-agent: *\nAllow: /\n");
 
+
+writeFileSync("site/sitemap.xml", "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n  <url><loc>https://mizcausevic-dev.github.io/telecom-routing-reliability-map/</loc></url>\n</urlset>\n");
